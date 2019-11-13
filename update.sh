46c01eb1dd2bd5ac9a8164ba7bce19d63f151d90
hudeneil <h7106019005@smail.nchu.edu.tw>
2019-11-13 15:11:12 +0000

    update_test

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+#git remote add web git@github.com:HuangDeneil/Web-test.git
+#git pull web master
+
+-
+
+retval=$?
+if [ $retval -eq 0 ]; then
+    echo "<h1>service started by script</h1>"
+else
+    echo "<h1>not able to start service </h1>"
+fi
+
+
+
+
+
